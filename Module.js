542dfac436edd2b7dec6200595fdf0df5c6c4ea6
Ayur67 <aur67@mail.ru>
2023-12-15 11:04:27 +0300

    create Model

--- Module.js
+++ Module.js
@@ -1,2 +1,30 @@
-console.log("sur");
-console.log("фыв");
+const fs = require("fs").promises;
+class Model {
+  constructor(score = 0, questions) {
+    this.themes = themes;
+    this.score = score;
+    this.currentQuestion = 0;
+    this.questions = [
+      {
+        question: "Какая самая высокая гора в мире?",
+        answer: "Эверест",
+      },
+      {
+        question: "Кто написал 'Войну и мир'?",
+        answer: "Лев Толстой",
+      },
+      {
+        question: "Какой химический элемент имеет символ 'O'?",
+        answer: "Кислород",
+      },
+      {
+        question: "Кто был первым президентом США?",
+        answer: "Джордж Вашингтон",
+      },
+    ];
+  }
+
+  nextQuestion() {
+    return (this.currentQuestion += 1);
+  }
+}
